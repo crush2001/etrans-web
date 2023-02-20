7b12a647802bd5df205124dcf7d92fe7d3c7c132
“ <201994460@qq.com>
2023-02-20 21:10:56 +0800

    改进“帮助与关于”界面

--- src/views/AdminView/AdminHelps.vue
+++ src/views/AdminView/AdminHelps.vue
@@ -10,7 +10,28 @@
       （4）js-md5，用于对用户数据进行md5加密，保证系统数据的安全性<br />
       （5）echarts，用于可视化图表的展示，使公司的概况和各项数据一目了然，提升管理效率<br /><br />
       对本系统管理员，提供数据可视化统计、员工管理、薪酬管理、公共交通线路管理、公共交通班次管理、公司资产管理（包括车辆管理和车站管理）、公告管理、会议管理和在线审批等功能<br /><br />
-      本系统作者为闽南师范大学计算机学院19级计算机科学与技术专业2班本科生翁辰龙，为本人的毕业设计项目<br /><br />
+      本系统作者为闽南师范大学计算机学院19级计算机科学与技术专业2班本科生翁辰龙，为本人的毕业设计项目<br />
+      代码开源地址：<br />
+      前端：<el-link icon="el-icon-upload" href="https://github.com/crush2001/etrans-web">E-Trans易运前端</el-link>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      后端：<el-link icon="el-icon-upload" href="https://github.com/crush2001/etrans-web">E-Trans易运后端</el-link>
+      <br /><br />
+      <div class="block">
+        <el-timeline>
+          <el-timeline-item timestamp="2022年6月" placement="top">
+            <el-card>
+              <h4>确定选题</h4>
+              <p>确定本人毕业设计选题为“E-Trans易运协同办公系统”，旨在实现一个前后端分离的、基于Vue2+Element UI+SpringBoot+MySQL+MyBatis的公共交通运营企业协同办公系统</p>
+            </el-card>
+          </el-timeline-item>
+          <el-timeline-item timestamp="2023年2月" placement="top">
+            <el-card>
+              <h4>完成代码编写和文档撰写</h4>
+              <p>完成前端Vue代码、后端SpringBoot代码编写并提交代码至Github，完成需求说明书、开题报告、毕业论文及其他辅助性文档编写</p>
+            </el-card>
+          </el-timeline-item>
+        </el-timeline>
+      </div>
       <el-divider></el-divider>
       <div style="text-align: center">
         Copyright ©2022-2023 E-Trans-OASystem All Rights Reserved.
